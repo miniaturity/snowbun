4fe6bb53106a6007c49fbda56e8a00c641480fa0
miniaturity <miniaturity2.o@gmail.com>
2025-11-25 15:50:00 -0500

    initial commit

---
+++ seabunny/src/lib/components/Button.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  export let btn: "small" | "medium" | "large";
+  export let text: string = "";
+  export let img: string = "";
+  export let href: string;
+  export let alt: string;
+  export let className: string;
+</script>
+
+<a aria-label={alt} data-title={alt} href={href} class={className + " " + btn}>
+  <div class="bg-wrapper">
+    {#if text}
+      <div class="btn-text">{text}</div>
+    {/if}
+
+    {#if img}
+      <div class="btn-img">
+        <img src={img} alt={alt}/>
+      </div>
+    {/if}
+  </div>
+</a>
+
+<style>
+.small { width: 100px; height: 100px; }
+.medium { width: 200px; height: 100px; }
+.large { width: 200px; height: 200px; }
+
+a { 
+  display: block; 
+  all: unset; 
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.btn-img {
+  display: flex;
+  align-items: flex-start;
+  width: 100%;
+  height: 100%;
+}
+
+img {
+  object-fit: contain;
+  width: auto; height: 100%;
+}
+
+.bg-wrapper {
+  display: block;
+  width: 100%; height: 100%;
+
+  --blur: 2px;
+  --white: rgba(255, 255, 255, 1);
+  --blackBorder: rgba(0, 0, 0, 0.05);
+
+  border: 0;
+  backdrop-filter: blur(4px);
+  box-shadow: 
+    0 -1px var(--blur) 0 var(--white),
+    0  1px var(--blur) 0 var(--white),
+    1px 1px var(--blur) 0 var(--blackBorder),
+   -1px 0  var(--blur) 0 var(--blackBorder),
+    0 0 10px rgba(0, 0, 0, 0.1);
+  background: rgba(255, 255, 255, 0.1);
+
+  transition: all 0.3s ease-in-out;
+}
+
+.bg-wrapper:hover {
+  backdrop-filter: none;
+}
+</style>
---
+++ seabunny/src/routes/+page.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import Button from "$lib/components/Button.svelte";
+  import mini from "$lib/assets/image.png";
+
+</script>
+
+<main>
+  <div class="buttons">
+    <Button 
+      btn="small"
+      href="https://miniaturity.com/" 
+      alt="creator (miniaturity)"
+      img={mini}
+      className="_a"
+    />
+    <Button 
+      btn="small"
+      href="https://miniaturity.com/"
+      alt="ph"
+      img={mini}
+      className="_b"
+    />
+    <Button 
+      btn="medium"
+      href="https://miniaturity.com/"
+      alt="ph"
+      img={mini}
+      className="_c"
+    />
+    <Button 
+      btn="large"
+      href="https://miniaturity.com/"
+      alt="ph"
+      text="testestestest"
+      className="_d"
+    />
+  </div>
+</main>
+
+<style lang="scss">
+  :global(body) {
+    margin: 0;
+    font-family: "Roboto", sans-serif;
+  }
+
+  main {
+    position: relative;
+    width: 100vw; height: 100vh;
+    background-image: linear-gradient(rgba(149, 149, 149, 0.5), rgba(149, 149, 149, 0.5)), url(../lib/assets/coralreef.gif);
+    background-size: cover;
+
+    display: grid;
+
+  }
+
+  .buttons {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+
+    width: 400px;
+    height: 400px;
+    align-self: center;
+    justify-self: center;
+  }
+
+  :global(._a) { grid-area: 1 / 1 / 2 / 2; }
+  :global(._b) { grid-area: 1 / 2 / 2 / 3; }
+  :global(._c) { grid-area: 2 / 1 / 3 / 3; }
+  :global(._d) { grid-area: 1 / 3 / 3 / 4; }
+</style>
