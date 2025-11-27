5d7ac09e0f415e837fcb2d16583027d1438f42c0
miniaturity <miniaturity2.o@gmail.com>
2025-11-26 20:51:26 -0500

    feat: directory layout

--- seabunny/src/lib/components/Zoo.svelte
+++ seabunny/src/lib/components/Zoo.svelte
@@ -1,43 +1,55 @@
-<style>
-    .bannerIcon {
-        vertical-align: -0.07em;
-        font-size: 24px !important;
-    }
-    @font-face {
-        font-family: 'Phantom Sans';
-        src: url('https://assets.hackclub.com/fonts/Phantom_Sans_0.7/Bold.woff')
-            format('woff'),
-            url('https://assets.hackclub.com/fonts/Phantom_Sans_0.7/Bold.woff2')
-            format('woff2');
-        font-weight: bold;
-        font-style: normal;
-        font-display: swap;
-    }
+<style lang="scss">
+	@use "../styles/globals";
 
-    #zoo_linkBanner {
-      background-color: rgb(29, 65, 29);
-      padding: 10px;
-      text-align: center;
-      vertical-align: center;
-      position: fixed;
-      left: 0;
-      right: 0;
-      top: 0;
-      height: 65px;
-      z-index: 1000;
-      font-size: 15px;
-      transition: box-shadow 0.3s ease-in-out;
-      font-family: Phantom Sans, sans-serif;
-      text-decoration: none;
-      color: white;
-    }
-    #zoo_linkBanner:hover {
-      cursor: pointer;
-      box-shadow: 0px 0px 50px 10px rgba(0, 0, 0, 0.551);
-    }
+	@font-face {
+			font-family: 'Phantom Sans';
+			src: url('https://assets.hackclub.com/fonts/Phantom_Sans_0.7/Bold.woff')
+					format('woff'),
+					url('https://assets.hackclub.com/fonts/Phantom_Sans_0.7/Bold.woff2')
+					format('woff2');
+			font-weight: bold;
+			font-style: normal;
+			font-display: swap;
+	}
+
+	#zoo_linkBanner {
+		background-color: #1F53FF;
+		padding: 10px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		position: fixed;
+		left: 0;
+		right: 0;
+		top: 0;
+		height: 50px;
+		z-index: 1000;
+		font-size: 1.4rem;
+
+		font-family: u001, sans-serif;
+		text-decoration: none;
+		color: white;
+
+		& span {
+			padding: 4px;
+		}
+
+		&:hover {
+			cursor: pointer;
+
+			& span {
+				background-color: globals.$hl;
+				color: #000;
+			}
+		}
+}
+	
 </style>
 
-<a href = "https://zoo.hackclub.com"><div id="zoo_linkBanner">
-    <h1>CHECK OUT MORE AT THE ZOO <span class="material-symbols-outlined bannerIcon">pets</span> </h1>
-</div></a>
+<a href = "https://zoo.hackclub.com">
+	<div id="zoo_linkBanner">
+			<span>check out more @ the zoo!</span>
+	</div>
+</a>
 <div style="height: 85px"></div>
--- seabunny/src/routes/+page.svelte
+++ seabunny/src/routes/+page.svelte
@@ -1,76 +1,214 @@
 <script lang="ts">
-  import ZooBanner from "$lib/components/Zoo.svelte";
-  import banner1 from "$lib/assets/seabunnyyt.webp";
-
-  let banner = banner1; 
+  import ZooBanner from "$lib/components/zoo.svelte";
+  import sbicon from "$lib/assets/images/sbicob.png";
+  import crbgn from "$lib/assets/images/crbgn.png";
+  import aqcn from "$lib/assets/images/aqcn.png";
 
 </script>
 
 <main>
   <ZooBanner />
   <header>
-    <nav>
-
-    </nav>
     <div id="banner">
-      <div style="flex-grow: 1;"></div>
-      <span>sea bunny</span>
-      <img src={banner} alt="banner" id="banner-img"/>
-      <div id="overlay"></div>
+      <div id="inner-banner">
+        <div>
+          <span id="header-text">sea bunny</span>
+          <img alt="seabunny" src={sbicon}/>
+        </div>
+      </div>
+      <img alt="coralreef" src={crbgn} id="banner-img"/>
     </div>
-    
   </header>
+  <div id="title">
+    <span>
+      welcome to the directory. find out more about sea bunnies!
+    </span>
+    <div>
+      <a href="https://miniaturity.com">
+        im feeling lucky
+      </a>
+      &nbsp;—&nbsp;
+      <a href="https://miniaturity.com">
+        creator
+      </a>
+      &nbsp;—&nbsp;
+      <a href="https://github.com/miniaturity">
+        github
+      </a>
+    </div>
+  </div>
+  <nav>
+    <button class="nav-button">
+      <div class="inner-nbutton">
+        <span>a</span>
+      </div>
+      <img src={aqcn} alt="" class="navbtn-img"/>
+    </button>
+    <button class="nav-button">
+      <div class="inner-nbutton">
+        <span>a</span>
+      </div>
+    </button>
+    <button class="nav-button">
+      <div class="inner-nbutton">
+        <span>a</span>
+      </div>
+    </button>
+    <button class="nav-button">
+      <div class="inner-nbutton">
+        <span>a</span>
+      </div>
+    </button>
+  </nav>
 </main>
 
 <style lang="scss">
   @use "../lib/styles/globals";
 
-  :global(:root) {
-    --margin: 4px;
-
-    --nav: #a0a0a0;
-  }
-
-  :global(body) {
-    margin: 0;
-    font-family: "Roboto", sans-serif;
-  }
-
   main {
     display: flex;
-    width: 50vw; height: 100vh;
+    width: 40vw; height: 100vh;
 
     flex-direction: column;
     align-items: center;
+    justify-content: center;
     justify-self: center;
     
     background: #fff;
   }
 
   header {
-    height: 50%;
-    width: 100%;
+    width: 100%; height: 40%;
+  }
+
+  #title {
+    width: 100%; height: 10%;
+    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    margin: calc(globals.$margin * 2);
+    gap: calc(globals.$margin * 2);
+
+    & span {
+      color: #000;
+      
+      font-size: 1.5rem;
+      font-weight: bold;
+    }
+
+    & div {
+      display: flex;
+      flex-direction: row;
+      transform: scale(.7, 1.2);
+      gap: globals.$margin;
+
+      & a {
+        height: fit-content;
+        font-size: 1.2rem;
+      }
+    }
+
   }
 
   nav {
-    height: 10%;
-    width: 100%;
+    width: 100%; height: 30%;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(4, 1fr);
 
-    background: var(--nav);
+    grid-row-gap: 8px; grid-column-gap: 8px;
+    margin: 0 0 calc(globals.$margin * 2) 0;
+
+    & .nav-button {
+      position: relative;
+      padding: 0;
+      background-color: #1F53FF;
+      border: none; outline: none;
+            
+      
+      &:nth-child(1) {
+        grid-area: 1 / 1 / 3 / 3;
+      }
+      &:nth-child(2) {
+        grid-area: 3 / 1 / 5 / 3;;
+      }
+      &:nth-child(3) {
+        grid-area: 3 / 3 / 5 / 5;
+      }
+      &:nth-child(4) {
+        grid-area: 1 / 3 / 3 / 5;
+      }
+
+    }
+
   }
+
+
+  .inner-nbutton {
+    height: 100%;
+    padding: calc(globals.$margin * 2.5);
+
+    display: flex;
+    align-items: center;
+
+    color: #fff;
+    font-weight: bold;
+    font-size: 1.5rem;
+
+    box-sizing: border-box;
+
+    & span { 
+      border: 1px solid #fff; 
+      width: 100%;
+      height: 100%;
+    }
+  }
+  
 
   #banner {
     position: relative;
     display: flex;
     flex-direction: column;
-    width: 100%; height: 80%;
-    
+    width: 100%; height: 100%;
+    background: globals.$main;
+    overflow: hidden;
+  }
 
-    & span {
+  #inner-banner {
+    margin: calc(globals.$margin * 2.5);
+    display: flex;
+    align-items: flex-end;
+    border: 1px solid #fff;
+    height: 100%;
+
+    & div {
+      display: flex;
+      flex-direction: column;
+      margin: 15px;
       z-index: 2;
-      margin: calc(var(--margin) * 3);
+    }
+
+    & div > #header-text {
       font-size: 3rem;
+      font-weight: bold;
       color: #fff;
+    }
+
+    & div > img {
+      position: absolute;
+      bottom: 10px;
+      right: 10px;
+      width: 30%;
+      height: auto;
+
+      transition: all 0.3s ease;
+
+      &:hover {
+        scale: 1.1;
+      }
     }
   }
 
@@ -79,15 +217,15 @@
     top: 0;
     left: 0;
     width: 100%; height: 100%;
-    object-fit: cover;
+    opacity: 0.5;
   }
 
-  #overlay {
+  .navbtn-img {
     position: absolute;
     top: 0;
     left: 0;
     width: 100%; height: 100%;
-    background: linear-gradient(180deg,rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.66) 90%);
+    opacity: 0.5;
   }
 
 </style>
