de122b2459253bd04129a1d84a4d76b521364c7c
miniaturity <miniaturity2.o@gmail.com>
2025-11-28 13:20:09 -0500

    feat: dialogue component

--- seabunny/src/lib/components/dialogue.svelte
+++ seabunny/src/lib/components/dialogue.svelte
@@ -1,24 +1,25 @@
 <script lang="ts">
-  const SPEED = 30;
+
+  const SPEED = 60;
 
   let {
     speaker,
     text,
-    avatar,
     onNext,
   }: {
     speaker: string,
     text: string,
-    avatar?: string,
     onNext: () => void,
   } = $props();
 
   let displayed = $state("");
-  let index = $state(0);
-  let interval: number | undefined = $state(undefined);
+  let index = 0;
+  let interval: number | undefined;
 
-  $effect(() => {
-    if (text) {
+  $effect.root(() => {
+    $effect(() => {
+      text;
+
       displayed = "";
       index = 0;
       clearInterval(interval);
@@ -31,33 +32,78 @@
           clearInterval(interval);
         }
       }, SPEED);
-    }
+    
+      return () => clearInterval(interval);
+    });
   });
 
 </script>
 
 <div class="dialogue-box">
   <div class="dialogue-title">
-    {#if avatar}
-      <img src={avatar} alt=""/>
-    {/if}
-
     <div class="dialogue-name">
       {speaker}
     </div>
   </div>
   <div class="dialogue-content">
-    {displayed}
-  </div>
-  <div class="dialogue-next">
-    {#if displayed === text}
-      <button onclick={onNext}>
-        {">>"}
-      </button>
-    {/if}
+    <div class="dialogue-text">
+      {displayed}
+    </div>
+    <div class="dialogue-next">
+      {#if displayed === text}
+        <button onclick={onNext}>
+          {">>"}
+        </button>
+      {/if}
+    </div>
   </div>
 </div>
 
 <style lang="scss">
+  @use "../styles/globals";
 
+  .dialogue-box {
+    display: flex;
+    width: 100%; height: 100%;
+    flex-direction: column;
+  }
+
+  .dialogue-title {
+    display: flex;
+    flex-direction: row;
+    width: 100%; height: fit-content;
+
+    background: globals.$main;
+    border: 1px solid globals.$main;
+    color: #fff;
+
+    padding: calc(globals.$margin * 2.5);
+  }
+
+  .dialogue-name {
+    font-weight: bold;
+    font-size: 1.7rem;
+  }
+
+  .dialogue-content {
+    display: flex;
+    flex-direction: column;
+    width: 100%; height: 85%;
+    justify-content: space-between;
+
+    border: 1px solid globals.$main;
+
+    padding: calc(globals.$margin * 2.5);
+
+  }
+
+  button {
+    border: none; outline: none; background: none;
+    color: globals.$main;
+
+    &:hover, &:focus {
+      cursor: pointer;
+      font-size: 1.3rem;
+    }
+  }
 </style>
--- seabunny/src/routes/exhibit/+page.svelte
+++ seabunny/src/routes/exhibit/+page.svelte
@@ -1,21 +1,33 @@
 <script lang="ts">
 	import Breadcrumbs from "$lib/components/breadcrumbs.svelte";
+	import Dialogue from "$lib/components/dialogue.svelte";
   import Header from "$lib/components/header.svelte";
+  
+  import realseabunny from "$lib/assets/images/realseabunny.png";
 
   const dialogues = [
-    "it sure is nice off the coast of indonesia! the algae is yummy",
-    "mmm im hungry lemme get sum toxic sponge",
-    "hi im sea bunny"
+    "hello i am sea bunny. welcome to the exhibit",
+    "it sure is nice off of the coast of indonesia. i blend in with the sponges and algae arouynd here.",
+    "i'm feeling hungry"
   ];
 
-  let completedDialogue = $state("");
-  let dialogue = $state<{ title: string, text: string, img: string }>({
-    title: "sea bunny",
-    text: dialogues[1],
-    img: "",
-  });
+  const answers = [
+    "my name is Tim Sweeney",
+    "*eats sponge* hey thanks",
+    ""
+  ];
 
-  
+  let text = $state(dialogues[0]);
+
+  const getRandomDialogue = (): void => {
+    let index = Math.floor(Math.random() * dialogues.length);
+    text = dialogues[index];
+  }
+
+  const getAnswer = (i: number): void => {
+    if (i < 0 || i >= answers.length) return;
+    text = answers[i];
+  }
 
 </script>
 
@@ -28,20 +40,34 @@
   </div>
   <div id="top-cols">
     <div id="dialoguebox">
-      <div id="d-title">
-        <div>
+        <Dialogue 
+          speaker="sea bunny"
+          text={text}
+          onNext={() => {getRandomDialogue()}}
+        />
+    </div>
+    <div class="exhibit-img">
+      <img src={realseabunny} alt=""/>
+    </div>
+  </div>
+  <div id="bottom-cols">
+    <div id="actions">
+      <button onclick={() => getAnswer(1)}>
+        feed
+      </button>
+      <button onclick={() => getAnswer(0)}>
+        whats your name
+      </button>
+    </div>
+    <div id="stats">
 
-        </div>
-      </div>
-      <div id="dialogue">
-        {dialogue.text}
-      </div>
     </div>
   </div>
 </main>
 
 
 <style lang="scss">
+  @use "../../lib/styles/globals";
   main {
     display: flex;
     flex-direction: column;
@@ -49,9 +75,39 @@
     justify-self: center;
 
     min-width: 400px;
+    gap: calc(globals.$margin * 2.5);
   }
 
   #hc {
     width: 100%; height: 20%;
+  }
+
+  #top-cols {
+    display: flex;
+    flex-direction: row;
+    width: 100%; height: 20%;
+    gap: calc(globals.$margin * 2.5);
+  }
+
+  #bottom-cols {
+    width: 100%; height: 40%;
+    margin-top: calc(globals.$margin * 2.5);
+  }
+
+  #dialoguebox {
+    display: flex;
+    flex-direction: row;
+    margin: calc(globals.$margin * 2.5);
+    height: 100%; width: 50%;
+  }
+
+  .exhibit-img {
+    margin: calc(globals.$margin * 2.5);
+    width: 50%; height: 100%;
+
+    & img {
+      width: 100%; height: 100%;
+      object-fit: cover;
+    }
   }
 </style>
